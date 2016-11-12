339c1fade0f3be06fad27bbb08bc76cb3706cde2
unknown <700benjie@gmail.com>
2016-11-12 10:12:44 -0400

    nuevo

---
+++ Ejemplo.js
@@ -0,0 +1,5 @@
+var Taller={Herramientas:"Martillo", Tipo:"carpinteria",Lugar:"El Alto",Publico:"SI"}
+console.log("Taller:"+Taller.Herramientas);
+console.log("Taller:"+Taller.Tipo);
+console.log("Taller:"+Taller.Lugar);
+console.log("Taller:"+Taller.Publico);
---
+++ tablaSuma.js
@@ -0,0 +1,10 @@
+function suma(numero)
+{
+	var x=7
+	for (var i = 0 ; i <= 10; i++)
+	 {
+	 	//x=x+i;
+	 	console.log(x+"+"+i+"="+(numero++));
+	 }
+}
+suma(7);
